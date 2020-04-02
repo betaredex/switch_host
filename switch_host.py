ffb44fef0f2e9826124d7e5e13e22fb159c9c4ef
Elliot Brock <beta.redex@gmail.com>
2020-04-02 14:44:18 -0400

    Fix bugs and get everything working

--- switch_host.py
+++ switch_host.py
@@ -1,6 +1,7 @@
 import config
 import requests
 import time
+import json
 
 current_server = "main"
 
@@ -8,15 +9,21 @@ while True:
     try:
         r = requests.get("http://" + config.main_ip, timeout=1)
         server_up = r.status_code == 200
-    except Timeout:
+    except:
         server_up = False
 
-    if not server_up and current_server == main:
-        r = requests.patch(config.api_url_1, headers=config.headers, data={"content":config.backup_ip})
-        r = requests.patch(config.api_url_2, headers=config.headers, data={"content":config.backup_ip})
-        current_server = backup
-    elif server_up and current_server == backup:
-        r = requests.patch(config.api_url_1, headers=config.headers, data={"content":config.main_ip})
-        r = requests.patch(config.api_url_2, headers=config.headers, data={"content":config.main_ip})
+    print("server_up: " + str(server_up))
+
+    if not server_up and current_server == "main":
+        print("switching to backup")
+        r1 = requests.patch(config.api_url_1, headers=config.headers, data=json.dumps({"content":config.backup_ip}))
+        r2 = requests.patch(config.api_url_2, headers=config.headers, data=json.dumps({"content":config.backup_ip}))
+        current_server = "backup"
+
+    elif server_up and current_server == "backup":
+        print("switching to main")
+        r1 = requests.patch(config.api_url_1, headers=config.headers, data=json.dumps({"content":config.main_ip}))
+        r2 = requests.patch(config.api_url_2, headers=config.headers, data=json.dumps({"content":config.main_ip}))
+        current_server = "main"
     
     time.sleep(1)
