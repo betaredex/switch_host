d6caec91bbe8c6d5a917aebb55da441da5426ada
Elliot Brock <beta.redex@gmail.com>
2020-04-07 14:56:08 -0400

    Add some documentation

--- switch_host.py
+++ switch_host.py
@@ -6,6 +6,7 @@ import json
 current_server = "main"
 
 while True:
+    # Check if the main server is up
     try:
         r = requests.get("http://" + config.main_ip, timeout=1)
         server_up = r.status_code == 200
@@ -13,12 +14,14 @@ while True:
         server_up = False
 
     if not server_up and current_server == "main":
+        # If the main server is down, switch to the backup server
         print("Switching to backup")
         for url in config.api_urls:
             r = requests.patch(url, headers=config.headers, data=json.dumps({"content":config.backup_ip}))
         current_server = "backup"
 
     elif server_up and current_server == "backup":
+        # If the main server is up, switch back to the main server
         print("Switching to main")
         for url in config.api_urls:
             r = requests.patch(url, headers=config.headers, data=json.dumps({"content":config.main_ip}))
